3de824d446f932123c6bc487424c7988d13395be
Amaury Deltenre <amaury.deltenre@gmail.com>
2021-05-12 10:31:37 +0200

    Add GameOverScene

---
+++ GameJam/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    #region unity messages
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    #endregion
+
+
+
+    #region public methods
+
+    public void LoadLevel(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    #endregion
+
+}
